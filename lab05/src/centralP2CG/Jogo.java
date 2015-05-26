bb06fc32dce5eb579697b0123b880a35b1e5d4ba
alexandreg <alexandreg@lcc2-28.lcc.ufcg.edu.br>
2015-05-26 08:48:25 -0300

    teste lab05

---
+++ lab05/src/centralP2CG/Jogo.java
@@ -0,0 +1,13 @@
+package centralP2CG;
+
+public class Jogo {
+	
+	private String nome;
+	private double preco;
+
+	public Jogo(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+}
